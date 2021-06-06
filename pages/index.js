b10370d80c86efce251318651c370c26b6446f6b
nadavschwartz <nadavschwartz58@gmail.com>
2021-06-06 10:12:35 -0700

    hero section for homePage

--- pages/index.js
+++ pages/index.js
@@ -153,7 +153,7 @@ const HomePage = ({ events }) => {
                         className="text-blueGray-700 fill-current"
                       ></polygon>
                     </svg>
-                    <h4 className="text-xl font-bold text-white">
+                    <h4 className="text-xl font-bold text-white text-center">
                       SHARE YOUR EVENTS
                     </h4>
                     <div className="mt-2 flex flex-wrap justify-between text-white">
@@ -163,10 +163,100 @@ const HomePage = ({ events }) => {
                       <i className="fab fa-pinterest-square text-3xl"></i>
                       <i className="fab fa-reddit-square text-3xl"></i>
                     </div>
-                  <h4 className="text-xl font-bold text-white mt-3">
-                    AND MANY MORE
-                  </h4>
+                    <h4 className="text-xl font-bold text-white mt-3 text-center">
+                      AND MANY MORE
+                    </h4>
                   </blockquote>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="relative py-20">
+          <div
+            className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
+            style={{ transform: "translateZ(0)" }}
+          >
+            <svg
+              className="absolute bottom-0 overflow-hidden"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+              version="1.1"
+              viewBox="0 0 2560 100"
+              x="0"
+              y="0"
+            >
+              <polygon
+                className="text-white fill-current"
+                points="2560 0 2560 100 0 100"
+              ></polygon>
+            </svg>
+          </div>
+
+          <div className="container mx-auto px-4">
+            <div className="items-center flex flex-wrap">
+              <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
+                <img
+                  alt="..."
+                  className="max-w-full rounded-lg shadow-lg"
+                  src="/about_us.svg"
+                />
+              </div>
+              <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
+                <div className="md:pr-12">
+                  <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-blueGray-200">
+                    <i className="fas fa-address-card text-xl"></i>
+                  </div>
+                  <h3 className="text-3xl font-semibold">ABOUT US</h3>
+                  <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
+                    DJE takes pride in helping and connecting between people
+                    through events
+                  </p>
+                  <ul className="list-none mt-6">
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
+                            <i className="fas fa-fingerprint"></i>
+                          </span>
+                        </div>
+                        <div>
+                          <h4 className="text-blueGray-500">
+                            By sharing your events on social media
+                          </h4>
+                        </div>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
+                            <i className="fab fa-html5"></i>
+                          </span>
+                        </div>
+                        <div>
+                          <h4 className="text-blueGray-500">
+                            Keep tracks of all your events - past and future in
+                            one dashboard
+                          </h4>
+                        </div>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
+                            <i className="far fa-paper-plane"></i>
+                          </span>
+                        </div>
+                        <div>
+                          <h4 className="text-blueGray-500">
+                            User and mobile friendly design
+                          </h4>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
